7073aa9104dbe75a767557f855e7a77f6e429e34
Kevin Kuchta <kevin@kevinkuchta.com>
2017-03-05 20:45:26 -0800

    Get super-simple react frontend working

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Generate the json
 
 1. DL the xml file from https://dumps.wikimedia.org/enwikivoyage/latest/enwikivoyage-latest-pages-articles.xml.bz2 and unzip it
-2. Run `( cd data_converter; ruby generate_json.rb ) > data/ancestors.json`
+2. Run `( cd data_converter; ruby generate_json.rb ) > public/ancestors.json`
 
 `# TODO: actual setup instructions here`
--- src/App.js
+++ src/App.js
@@ -1,20 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import _  from 'lodash';
+//import logo from './logo.svg';
 import './App.css';
 
+fetch('/ancestors.json')
+  .then( (response) => response.json() )
+  .then( (json) => {
+    console.log('loaded json');
+    window.ancestorData = json
+  });
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { pageTitle: '' };
+  }
+  newRandom = () => {
+    console.log('App.newRandom called with this=', this);
+    const newPageTitle = _(window.ancestorData).keys().sample();
+    console.log('newPageTitle=', newPageTitle);
+    console.log('{ pageTitle: newPageTitle }', { pageTitle: newPageTitle });
+    this.setState({ pageTitle: newPageTitle });
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        pageTitle= {this.state.pageTitle}
+        <Tree />
+        <RandomButton newRandom={this.newRandom.bind(this)} />
+        <Link pageTitle={this.state.pageTitle} />
       </div>
     );
+  }
+}
+
+class Tree extends Component {
+  render() {
+    return (
+      <div>
+        Tree Here
+      </div>
+    )
+  }
+}
+
+class RandomButton extends Component {
+  render() {
+    return (
+      <button onClick={this.props.newRandom}>
+        Random Button
+      </button>
+    );
+  }
+}
+
+class Link extends Component {
+  url = (pageTitle) => {
+    return "https://en.wikivoyage.org/wiki/" + pageTitle;
+  }
+  render() {
+    return (
+      <iframe className='wiki-frame' src={this.url(this.props.pageTitle)} />
+      //<div>
+        //<a href={this.url(this.props.pageTitle)}>{this.url(this.props.pageTitle)}</a>
+      //</div>
+    )
   }
 }
 
