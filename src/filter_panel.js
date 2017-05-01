94670605c9fb332d07b95c5d542d29be7a417b9c
Kevin Kuchta <kevin@kevinkuchta.com>
2017-04-30 18:47:14 -0700

    Initial mobile design (busted on actual ios, though)

--- src/filter_panel.js
+++ src/filter_panel.js
@@ -16,6 +16,7 @@ class FilterPanel extends Component {
           <i className='compactShow fa fa-caret-left' />
           <i className='expandedShow fa fa-caret-down' />
         </div>
+        <div className='divider'></div>
       </div>
     )
   }
