b8291588ff0aea9a368a3a1acf714405b11d2f14
Kevin Kuchta <kevin@kevinkuchta.com>
2017-12-27 14:23:09 -0500

    Add www to non-www redirect

--- publish.sh
+++ publish.sh
@@ -1,3 +1,7 @@
 # usage:
 # AWS_PROFILE=whatever sh ./publish.sh
+npm run build
 aws s3 cp build s3://voyage-found/ --recursive --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+
+# Note: you'll also needs to set up a cloudfront invalidation manually at this
+# point since I'm too lazy to set up an aim role that allows that right now.
--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+// Redirect www to non-www.  This should be done in infrastructure, but I got
+// tired of trying to get route 53, s3 website redirects, and cloudfront to work
+// together correctly.
+if (window.location.hostname.match(/^www/)) {
+  window.location.href = window.location.href.replace('www.', '');
+}
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
